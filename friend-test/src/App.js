a0a496f9eeb7170f4fef13cc83f399689db0e441
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2021-11-20 10:22:50 +0900

    friend-test-commit

--- friend-test/src/App.js
+++ friend-test/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+import {Route} from "react-router-dom";
+
+import Start from "./Start";
+import Quiz from "./Quiz";
+import Score from "./Score";
+
 function App() {
+  const [name, setName] = React.useState("르탄이");  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        maxWidth: "350px",
+        margin: "auto",
+      }}
+    >
+      <Route path="/" exact>
+        <Start name={name} />
+      </Route>
+
+      <Route path="/quiz" exact>
+        <Quiz />
+      </Route>
+
+      <Route path="/score" exact>
+        <Score name={name} />
+      </Route>
     </div>
   );
 }
